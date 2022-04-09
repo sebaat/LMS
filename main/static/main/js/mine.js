d24d28ef6d4d2e650c4c29839313654181fa60b6
my condor <chakib>
2022-04-09 19:40:53 +0100

    Use the message framework for the first time.

---
+++ main/static/main/js/mine.js
@@ -0,0 +1,6 @@
+function hideMessages() {
+    let messages = document.getElementById("messages");
+    messages.hidden = true
+}
+
+setTimeout(hideMessages, 2000)
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from main.forms import CustomAuthenticationForm
-
+from django.contrib import messages
 
 
 @login_required
@@ -13,3 +13,8 @@ def profile(request):
 class CustomLoginView(LoginView):
     form_class = CustomAuthenticationForm
     next_page = "profile"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Successful login mister " + str(form.get_user().username),
+                         extra_tags="alert alert-success")
+        return super().form_valid(form)
